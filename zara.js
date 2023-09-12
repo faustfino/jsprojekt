4ef58d2e86b50b4c03ef92064f01da9655cbaf7a
Camipojep <125866708+Camipojep@users.noreply.github.com>
2023-09-12 23:08:00 +0200

    Update zara.js

--- zara.js
+++ zara.js
@@ -1,5 +1,5 @@
 function search_clothes() {
-    let input = document.getElementById('searchbar').value
+    let input = document.getElementById('search').value
     input=input.toLowerCase();
     let x = document.getElementsByClassName('clothes');
 
@@ -12,3 +12,27 @@ function search_clothes() {
         }
     }
 }
+
+let search = document.getElementById('search'); 
+let box = document.querySelectorAll('.box');
+search.addEventListener('keyup',(e) => {
+    if (e.key == "Enter") {
+     let inputvalue = search.value.toLowerCase();
+     box.forEach((image)=>{
+        if(inputvalue === image.dataset.name){
+            image.style.display = 'block';
+
+        } else {
+            image.style.display = 'none';
+        }
+     })
+    }
+})
+search.addEventListener('keyup',() => {
+    let inputvalue = search.value.toLowerCase();
+    if(inputvalue !="") return;
+    box.forEach((image)=>{
+        image.style.display = 'block';
+
+    })
+})
